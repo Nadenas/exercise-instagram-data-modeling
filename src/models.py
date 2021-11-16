e1c3a00c4c0f779b57fb4e2693c2b9d8ed5a3b06
Almudena De La Rosa <almudenaxico@gmail.com>
2021-11-16 11:26:04 +0000

    ok

--- src/models.py
+++ src/models.py
@@ -8,31 +8,57 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+  
+class Followers(Base):
+    __tablename__ = 'followers'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    user_to = Column(Integer)
+    user_from = Column(Integer)
+
+
+class Likes(Base):
+    __tablename__ = 'likes'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    like = Column(Integer)
+       
+
+class Media(Base):
+    __tablename__ = 'media'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    type = Column(String(250), nullable=False)
+    url = Column(String(250))
+    tag = Column(String(250))
+    likes_id = Column(Integer, ForeignKey('likes.id'))
+    likes = relationship(Likes)
+
+class Post(Base):
+    __tablename__ = 'post'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    text = Column(String(250), nullable=False)
+    media_id = Column(Integer, ForeignKey('media.id'))
+    media = relationship(Media) 
+      
+
+class Profile(Base):
+    __tablename__ = 'profile'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
-
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
-
-    def to_dict(self):
-        return {}
-
-## Draw from SQLAlchemy base
-try:
-    result = render_er(Base, 'diagram.png')
-    print("Success! Check the diagram.png file")
-except Exception as e:
-    print("There was a problem genering the diagram")
-    raise e
+    email = Column(String(250), nullable=False)
+    username = Column(String(250), nullable=False)
+    bio = Column(String(250))
+    post_id = Column(Integer, ForeignKey('post.id'))
+    post = relationship(Post)
+    user_to_id = Column(Integer, ForeignKey('followers.id'))
+    followers = relationship(Followers)
+    user_from_id = Column(Integer, ForeignKey('followers.id'))
+    followers= relationship(Followers)
